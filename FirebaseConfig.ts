acfc6b4ae1105b2abbec76b97e39f60ad1c6ee8b
EmilianoTL <atorresl1900@gmail.com>
2025-06-10 08:34:08 -0600

    HotFix para que funcione en Web

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeAuth/*, getReactNativePersistence */} from "firebase/auth";
+//import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP/*, {
   persistence: getReactNativePersistence(AsyncStorage)
-});
+}*/);
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
